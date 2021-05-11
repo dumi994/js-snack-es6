8de5906b8721c702a584a3b53dc16d8839f62980
dumi994 <dumi.9944@gmail.com>
2021-05-11 17:44:43 +0200

    creato array di bici

---
+++ snack1/main.js
@@ -0,0 +1,32 @@
+//Snack 1:
+//Creare un array di oggetti, ogni oggetto descriverà una bici da corsa con le seguenti proprietà: -nome -peso
+
+let bici = [
+    {
+        nome:'MTB Conor',
+        peso: 6,
+    },
+    {
+        nome: 'ELOPS 520',
+        peso: 7.5,
+    },
+    {
+        nome: 'DURANGO',
+        peso: 6,
+    },
+    {
+        nome:'BTWIN' ,
+        peso: 11,
+    },
+    {
+        nome:"VIVOBIKE" ,
+        peso: 10,
+    },
+    {
+        nome: 'SMOBY',
+        peso: 1.5,
+    },
+
+]
+
+//Stampare a schermo la bici con peso minore utilizzando destructuring e template literal
