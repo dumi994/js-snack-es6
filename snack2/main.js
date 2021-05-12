ddd5dedcc06760d8b46fd02fc1a92ef900f3f14f
dumi994 <dumi.9944@gmail.com>
2021-05-12 15:50:48 +0200

    perché non si pushano i falli subiti casuali?

--- snack2/main.js
+++ snack2/main.js
@@ -18,25 +18,41 @@ let squadre = [
     },
     {
         nome: 'Roma Primavera',
-        punti:0,
+        punti: 0,
         falliSubiti: 0,
     },
 ];
 console.log(squadre);
 //Generare numeri random al posto degli 0 nelle proprietà: Punti fatti e falli subiti.
 function getRandomNUmber(min, max){
-    return Math.random() * (max - min) + min;   
+    return Math.ceil(Math.random() * (max - min)) + min;   
 };
+getRandomNUmber(1, 100)
 console.log(parseInt(getRandomNUmber(1, 100)));
 
-squadre.falliSubiti += getRandomNUmber;
- 
+let questaSquadra;
+for (let i = 0; i < squadre.length; i++){
+    questaSquadra = squadre[i];
+    
+    if (questaSquadra.falliSubiti == 0){
+        getRandomNUmber(1, 100)
+    }
+    const puntiSquadra = {nome, falliSubiti} = questaSquadra;
+    console.log(puntiSquadra);
+    questaSquadra.falliSubiti = getRandomNUmber(1, 100);
+    console.log(questaSquadra.falliSubiti);
+}
 
-let falli = squadre.falliSubiti;
+
+/* 
+  */
+
+/* let falli = squadre.falliSubiti;
 let punti = squadre.punti;
-console.log(falli + punti);
+console.log(falli + punti); */
 /* console.log(squadre.number); */
 //Infine usando la destrutturazione creiamo un nuovo array i cui elementi contengono solo nomi e falli subiti e stampiamo tutto in console.
+
 
 
 
