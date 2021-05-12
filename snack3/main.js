1397701960b384d55c554a9e493e35c918cd7339
dumi994 <dumi.9944@gmail.com>
2021-05-12 17:24:06 +0200

    la funzione ritorna il nuovo array coi valori nel mezzo

--- snack3/main.js
+++ snack3/main.js
@@ -1,12 +1,25 @@
 //Snack 3:
 //Scrivere una funzione che accetti tre argomenti, un array e due numeri (a più piccolo di b).
-let array = [];
 
+const arrayNumbers = [1, 2, 4, 6, 8, 12, 19];
+const minNum = 1;
+const numMax  = 5;
+numeri (minNum, numMax, arrayNumbers)
 
-array.forEach((arrayo, i, a) => {
-    console.log(arrayo.length);
-});
+function numeri(min, max, array){
+    let newArray = [];
+    array.forEach((element, index) => {
+        console.log(element, index);
+        if (index <= max && index >= min){
+            newArray.push(element)
+        }
+    });
+    console.log(newArray);
+    document.getElementById('nuovoArray').innerHTML = `Il nuovo array contentiene i numeri compresi tra il più piccolo e il
+    più grande del vecchio array e sono: ${newArray}`
 
+};
+document.getElementById('vecchioArray').innerHTML = `Il tuo vecchio array è composto dai seguenti numeri: ${arrayNumbers}`
 
 //La funzione ritornerà un nuovo array con i valori che hanno la posizione compresa tra i due numeri.
 
